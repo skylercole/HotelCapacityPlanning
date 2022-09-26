4440fa7c2c999a64a9ee2a8abcd54a20b992d0a9
Stanislav Stoyanov-Pitkänen <stanislav.stoyanov@gmail.com>
2022-09-26 13:16:08 +0300

    Simplify Program.cs

--- HotelCapacityPlanning/Program.cs
+++ HotelCapacityPlanning/Program.cs
@@ -1,20 +1,14 @@
 ﻿using HotelCapacityPlanning;
 
-internal class Program
+string? jsonName = null;
+if (args.Length > 0)
 {
-    static void Main(string[] args)
-    {
-        string? jsonName = null;
-        if (args.Length > 0)
-        {
-            jsonName = Environment.GetCommandLineArgs()[1];
-        }
-        else
-        {
-            Console.WriteLine("Please enter as command line parameter an input json file.");
-            return;
-        }
-
-        Booking.PlanCapacity(jsonName);
-    }    
+    jsonName = Environment.GetCommandLineArgs()[1];
 }
+else
+{
+    Console.WriteLine("Please enter as command line parameter an input json file.");
+    return;
+}
+
+Booking.PlanCapacity(jsonName);
